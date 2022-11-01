8134bcce56baea7726ac5a480f9172a393277c4f
Luis Cerutti <luissantimcerutti@hotmail.com>
2022-10-31 21:06:51 -0300

    Primeiro commit

---
+++ teste.projeto/src/test/java/teste/projeto/TesteGameMania.java
@@ -0,0 +1,43 @@
+package teste.projeto;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteGameMania {
+
+	private WebDriver driver;
+	
+	
+	//Inicio
+	@Before
+	public void Configurarteste() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+	}
+	
+	//teste
+	@Test
+	public void TesteNavegabilidade () {
+		driver.get("https://informatica.sp.senai.br/");
+		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("Excel");
+		//driver.findElement(By.id("Busca1_btnBusca")).click();
+		driver.findElement(By.id("Busca1_btnBusca")).sendKeys(Keys.ENTER);
+	}
+		
+	//finalização
+	@After
+		
+	
+	
+	
+	
+}
+
